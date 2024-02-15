5adc222ac4010a53a519c992438809b297cabf2a
OlliDev <oliver.koenig.bi@gmail.com>
2024-02-15 15:45:35 +0100

    Add animations to player

--- player/player.gd
+++ player/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 
 @export var movement_speed: float
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 func _physics_process(delta: float) -> void:
 	
 	var input_vector: Vector2
@@ -12,3 +14,10 @@ func _physics_process(delta: float) -> void:
 	
 	velocity = input_vector * movement_speed
 	move_and_slide()
+	animate(input_vector)
+
+func animate(input_vector: Vector2) -> void:
+	if input_vector == Vector2.ZERO:
+		animation_player.play("idle")
+	else:
+		animation_player.play("run")
