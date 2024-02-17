933ddb72f18e777fe01cd04f50a5d1f8964c6015
OlliDev <oliver.koenig.bi@gmail.com>
2024-02-17 04:08:03 +0100

    Add Room progression

---
+++ game.gd
@@ -0,0 +1,41 @@
+class_name Game
+extends Node2D
+
+const room_list_path:= "res://rooms/list/"
+
+@onready var camera: PhantomCamera2D = $Camera
+@onready var room_holder: Node2D = $RoomHolder
+
+var current_room: int
+
+var _room_files: PackedStringArray
+
+func _ready() -> void:
+	var rooms_dir:= DirAccess.open(room_list_path)
+	_room_files = rooms_dir.get_files()
+	enter_room()
+
+func enter_room() -> void:
+	
+	if current_room >= _room_files.size():
+		print("no more rooms")
+		return
+	
+	var room_scene:= load(room_list_path + _room_files[current_room]) as PackedScene
+	var room:= room_scene.instantiate() as Node2D
+	
+	if room_holder.get_child_count() > 0:
+		var prev_room = room_holder.get_child(0)
+		room_holder.remove_child(prev_room)
+		prev_room.queue_free()
+	
+	room_holder.add_child(room)
+	camera.set_limit_node(room)
+	camera.update_limit_all_sides()
+	room.get_node("Door").player_entered.connect(_on_room_exited)
+
+func _on_room_exited() -> void:
+	Transitions.start_transition(Transitions.BLACK_FADE, 1, func():
+		current_room += 1
+		enter_room()
+		Transitions.end_transition(Transitions.BLACK_FADE, 1, func(): return))
---
+++ singletons/transitions/transitions.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+const BLACK_FADE:= "black_fade"
+
+var is_transitioning: bool
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func start_transition(trans_name: String, speed: float, on_finished: Callable):
+	is_transitioning = true
+	get_tree().root.set_process_input(false)
+	animation_player.play(trans_name + "_start", -1, speed)
+	await animation_player.animation_finished
+	if on_finished:
+		on_finished.call()
+
+func end_transition(trans_name: String, speed: float, on_finished: Callable):
+	animation_player.play(trans_name + "_end", -1, speed)
+	await animation_player.animation_finished
+	get_tree().root.set_process_input(true)
+	is_transitioning = false
+	if on_finished:
+		on_finished.call()
